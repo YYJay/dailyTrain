a7a003bad7fb4e25e60383eda7a45a5103f6f285
yingyuanjun <675363017@qq.com>
2018-09-04 15:37:20 +0800

    :star: 增加mongodb

--- Koa/server.js
+++ Koa/server.js
@@ -1,24 +1,31 @@
-// 引入模块
 import Koa from 'koa'
 import KoaStatic from 'koa-static'
 import Router from 'koa-router'
 import bodyParser from 'koa-bodyparser'
 
+import { database } from './mongodb' // 引入mongodb
+import { saveInfo, fetchInfo } from './controllers/info' // 引入info controller
+import { saveStudent, fetchStudent, fetchStudentDetail } from './controllers/student' // 引入 student controller
+
+database() // 链接数据库并且初始化数据模型
+
 const app = new Koa()
 const router = new Router()
 
-// 使用 bodyParser 和 KoaStatic 中间件
 app.use(bodyParser())
 app.use(KoaStatic(`${__dirname}/public`))
 
-// 路由设置test
 router.get('/test', (ctx, next) => {
   ctx.body = 'test page'
 })
 
-router.get('/', (ctx, next) => {
-  ctx.body = 'index page'
-})
+// 设置每一个路由对应的相对的控制器
+router.post('/saveinfo', saveInfo)
+router.get('/info', fetchInfo)
+
+router.post('/savestudent', saveStudent)
+router.get('/student', fetchStudent)
+router.get('/studentDetail', fetchStudentDetail)
 
 app
   .use(router.routes())
